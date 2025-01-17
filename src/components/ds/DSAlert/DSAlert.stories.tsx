4a8ad969242b8171fa8e2e26027418cd11f4715d
kakimaru <memimei0306@gmail.com>
2025-01-17 10:03:44 +0900

    feat: implement alert

--- src/components/ds/DSAlert/DSAlert.stories.tsx
+++ src/components/ds/DSAlert/DSAlert.stories.tsx
@@ -1,11 +1,18 @@
 import { Meta, StoryObj } from "@storybook/react";
-import DSAlert from "./DSAlert";
+import DSAlert, { colors } from "./DSAlert";
 
 const meta = {
   component: DSAlert,
   args: {
-    children: "react",
+    title: "Alert title",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officiis, quae tempore necessitatibus placeat saepe."
   },
+  argTypes: {
+    colors: {
+      control: "radio",
+      options: colors
+    }
+  }
 } satisfies Meta<typeof DSAlert>;
 
 export default meta;
--- src/components/ds/DSAlert/DSAlert.tsx
+++ src/components/ds/DSAlert/DSAlert.tsx
@@ -1,3 +1,35 @@
-export default function DSAlert() {
-  return <div>DSAlert</div>;
+import "./DSAlert.scss";
+import { RiErrorWarningLine } from "react-icons/ri";
+
+export const colors = ["primary", "warning", "danger"] as const;
+
+type Colors = (typeof colors)[number];
+
+export type Props = {
+  colors?: Colors;
+  title?: string;
+  description: string;
+};
+
+const BASE_CLASS = "ds-alert";
+
+export default function DSAlert({
+  colors = "primary",
+  title,
+  description,
+  ...rest
+}: Props) {
+  const classes = [BASE_CLASS, `${BASE_CLASS}--${colors}`]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <div className={classes} {...rest}>
+      <div className={`${BASE_CLASS}--header`}>
+        <RiErrorWarningLine />
+        <h1 className={`${BASE_CLASS}--title`}>{title}</h1>
+      </div>
+      <p className={`${BASE_CLASS}--description`}>{description}</p>
+    </div>
+  );
 }
